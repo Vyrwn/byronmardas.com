1373278fc0771fb5f86e669fc18e5a2de48b012a
Byron <byronmardas@gmail.com>
2018-09-28 15:41:54 +0100

    fix

--- php/form-process.php
+++ php/form-process.php
@@ -1,6 +1,5 @@
 <?php
 
-<<<<<<< HEAD
 $errorMSG = "";
 
 // NAME
@@ -9,20 +8,6 @@ if (empty($_POST["name"])) {
 } else {
     $name = $_POST["name"];
 }
-=======
-if(isset($_POST['submit']))
-{
-$name = $_POST['name'];
-$submit_links = $_POST['submit_links'];
-$from_add = "byron@byronmardas.com";
-$to_add = "byron@byronmardas.com";
-$subject = "Your Subject Name";
-$message = "Name:$name \n Sites: $submit_links";
-
-$headers = 'From: byron@byronmardas.com' . "\r\n" .
-
-'Reply-To: byron@byronmardas.com' . "\r\n";
->>>>>>> a0eb69d2fb3f734b18d473f967f09ec2647dcb11
 
 // EMAIL
 if (empty($_POST["email"])) {
